f38421c6cf34358a537143e44497a0166beaff7c
Gabarros <gabarros17@gmail.com>
2019-11-21 13:57:46 -0200

    Updated files

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const ClientController = require('./controllers/ClientController')
+const ClientController = require('./controllers/UserController')
 ;
 const routes = express.Router();
 
